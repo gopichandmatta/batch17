3d657b32098de80e4b89d77847bb6a962e7d1e1b
Harish <harishsomsole@gmail.com>
2024-02-09 17:25:02 +0530

    File handling, exception handling, decorators

--- oops.py
+++ oops.py
@@ -181,3 +181,79 @@
 # print(c1._Car__name)
 # print(c1.name)
 # print(c1.color)
+
+
+# def func(a):
+#     try:
+#         print(2 / a)
+#     except ZeroDivisionError as ex:
+#         print("a value should not be zero")
+#     else:
+#         print("No errors found")
+#     finally:
+#         print("Executes every time")
+#
+# func(4)
+# func(0)
+
+
+# file handling
+# read, open, write, close, update
+# r, w, r+, w+ , a+, rb, wb
+
+# f = open(filepath, mode)
+
+# f = None
+# try:
+#     f = open("file1.txt", mode='r')
+#     f.write("Harish")
+#     print(f.tell())
+#     f.seek(2)
+#     print(f.readlines())
+# except FileNotFoundError:
+#     print("File not found")
+# finally:
+#     if f:
+#         f.close()
+
+# context manager
+# with open("file.txt",mode='a+') as f:
+#     f.write("Harish 3")
+
+import time
+
+
+# Decorator is a function which takes another function
+# as an argument and extends its functionality without
+# modifying its source code.
+# def execution_time(func):
+#     def inner(lst, val):
+#         st = time.time()
+#         func(lst, val)
+#         et = time.time()
+#         print(et - st)
+#         print("Current function",func.__name__)
+#
+#     return inner
+# #
+# #
+# @execution_time
+# def linear_search(lst, val):
+#     for i in lst:
+#         if i == val:
+#             print("Value found")
+#             break
+#
+# @execution_time
+# def binary_search(lst, val):
+#     for i in lst:
+#         if i == val:
+#             print("Value found")
+#             break
+#
+# lst = [x for x in range(10000)]
+# linear_search(lst, 100)
+# binary_search(lst, 100)
+#
+
+
